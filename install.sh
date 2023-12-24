3362e90b2d03bf88060cfe7cc23b4e04406647a2
David Swanlund <10473778+TheTinHat@users.noreply.github.com>
2023-12-24 07:43:23 -0800

    Add install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+host=$1
+
+if [ -z "$host" ]; then
+  echo "Host not specified."
+  exit 1
+fi
+
+if [ ! -d "hosts/${host}" ]; then
+  echo "Cannot find directory for host directory"
+  exit 1
+fi
+
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko hosts/${host}/disko-config.nix
+nixos-generate-config --no-filesystems --root /mnt --dir hosts/${host}/
+git add .
+
+sudo nixos-install --flake ./hosts/#${host}
+
