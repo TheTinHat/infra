cf042cd81f27ee853828546d7a783a9a8cb15c44
David Swanlund <10473778+TheTinHat@users.noreply.github.com>
2023-12-24 08:17:40 -0800

    Cut out xorg from testbox

--- hosts/testbox/configuration.nix
+++ hosts/testbox/configuration.nix
@@ -20,37 +20,4 @@
   boot.loader.grub.efiInstallAsRemovable = true;
 
   networking.hostName = "testbox"; # Define your hostname.
-  networking.networkmanager.enable = true;
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "david";
-
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
-
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
 }
