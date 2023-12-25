96c99c792e93b117d207e4b5498aa5d152620e27
David Swanlund <10473778+TheTinHat@users.noreply.github.com>
2023-12-25 00:12:40 -0800

    Give testbox a DE

--- hosts/testbox/configuration.nix
+++ hosts/testbox/configuration.nix
@@ -4,7 +4,7 @@
       ./hardware-configuration.nix
       ./disko-config.nix
       ../../roles/common.nix
-      #../../roles/desktop.nix
+      ../../roles/desktop.nix
       ../../users/david.nix
       ../../roles/allow_ssh.nix
     ];
@@ -17,4 +17,37 @@
   boot.loader.grub.efiInstallAsRemovable = true;
 
   networking.hostName = "testbox"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Enable automatic login for the user.
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "david";
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+
+  # Printer
+  services.printing.enable = true;
+
+  # Sound
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 }
