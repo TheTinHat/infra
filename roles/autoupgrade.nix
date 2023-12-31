00ec08a1c808cdaa88188d45e8a30c41a620c91d
David Swanlund <10473778+TheTinHat@users.noreply.github.com>
2023-12-31 15:19:26 -0800

    Adjust autoupgrade

--- roles/autoupgrade.nix
+++ roles/autoupgrade.nix
@@ -1,13 +1,7 @@
 { ... }: {
   system.autoUpgrade = {
     enable = true;
-    flake = inputs.self.outPath;
-    flags = [
-      "--update-input"
-      "nixpkgs"
-      "-L" # print build logs
-    ];
     dates = "02:00";
-    randomizedDelaySec = "45min";
+    randomizedDelaySec = "30min";
   };
 }
