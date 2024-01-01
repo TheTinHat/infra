717da88b295754550a853c7b521cafb1f3819076
David Swanlund <10473778+TheTinHat@users.noreply.github.com>
2023-12-31 17:05:48 -0800

    Add timeoutstopsec

--- hosts/monitoring/configuration.nix
+++ hosts/monitoring/configuration.nix
@@ -43,5 +43,6 @@
   systemd.services.uptime-kuma.after = [ "network.target" "nfs-client.target" ];
   systemd.services.uptime-kuma.serviceConfig = {
     ReadWritePaths = "/mnt/appdata/kuma/";
+    TimeoutStopSec = 30;
   };
 }
