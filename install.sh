0a8ede08681c63e6419d9de7b655ba09f3eaf1c8
David Swanlund <10473778+TheTinHat@users.noreply.github.com>
2024-02-08 21:32:15 -0800

    adjust install script

--- install.sh
+++ install.sh
@@ -3,22 +3,21 @@
 host=$1
 
 if [ -z "$host" ]; then
-  echo "Host not specified."
-  exit 1
+	echo "Host not specified."
+	exit 1
 fi
 
 if [ ! -d "hosts/${host}" ]; then
-  echo "Cannot find directory for host directory"
-  exit 1
+	echo "Cannot find directory for host directory"
+	exit 1
 fi
 
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko hosts/${host}/disko-config.nix
 nixos-generate-config --no-filesystems --root /mnt --dir hosts/${host}/
 git add .
 
-sudo nixos-install --flake ./hosts/#${host}
+sudo nixos-install --flake ./#${host}
 
 sudo mkdir -p /mnt/etc/nixos/
 sudo git clone https://github.com/TheTinHat/infra.git /mnt/etc/nixos/infra
 sudo cp hosts/${host}/hardware-configuration.nix /mnt/etc/nixos/infra/hosts/${host}/
-
