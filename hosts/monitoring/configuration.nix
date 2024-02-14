6ce169e495373dd5eb6265183694fb33671da6e5
David Swanlund <10473778+TheTinHat@users.noreply.github.com>
2024-02-14 13:38:58 -0800

    use full domain

--- hosts/monitoring/configuration.nix
+++ hosts/monitoring/configuration.nix
@@ -41,7 +41,7 @@
 
     caddy = {
       enable = true;
-      virtualHosts."monitoring".extraConfig = ''
+      virtualHosts."monitoring.wolf-atlas.ts.net".extraConfig = ''
         reverse_proxy http://127.0.0.1:3001
       '';
     };
