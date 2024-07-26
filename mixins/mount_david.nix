9c13d46aa22bc27ae72b9080b71ce00ca0a7c1c5
David Swanlund <10473778+TheTinHat@users.noreply.github.com>
2024-07-26 10:45:25 -0700

    Update mount

--- mixins/mount_david.nix
+++ mixins/mount_david.nix
@@ -6,6 +6,6 @@
   fileSystems."/mnt/david" = {
     device = "//192.168.1.200/david";
     fsType = "cifs";
-    options = [ "x-systemd.automount" "noauto" ];
+    options = [ "credentials=/etc/nixos/smb-secrets" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=60" "x-systemd.device-timeout=5s" "x-systemd.mount-timeout=5s" ];
   };
 }
