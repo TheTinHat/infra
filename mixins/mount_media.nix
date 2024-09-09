6f7b39715aba06cc7c25087320a4c2e483e1bc52
David Swanlund <10473778+TheTinHat@users.noreply.github.com>
2024-09-08 20:35:52 -0700

    Set UID to 1000 on cifs shares

--- mixins/mount_media.nix
+++ mixins/mount_media.nix
@@ -6,6 +6,6 @@
   fileSystems."/mnt/media" = {
     device = "//192.168.1.200/media";
     fsType = "cifs";
-    options = [ "credentials=/etc/nixos/smb-secrets" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=60" "x-systemd.device-timeout=5s"  "x-systemd.mount-timeout=5s" "uid=1000" ];
+    options = [ "credentials=/etc/nixos/smb-secrets" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=60" "x-systemd.device-timeout=5s" "x-systemd.mount-timeout=5s" "uid=1000" ];
   };
 }
